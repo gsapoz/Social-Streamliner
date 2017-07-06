2222287f83fd48a0411e7d84329c85d952dc3c98
Gary <garysapozhn@gmail.com>
2017-07-06 16:08:59 -0400

    pod init

---
+++ SStreamliner/SStreamliner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SStreamliner
+//
+//  Created by GaryS on 7/6/17.
+//  Copyright © 2017 Gary Sapozhnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
